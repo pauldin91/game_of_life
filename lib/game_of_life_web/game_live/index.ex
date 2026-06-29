818d32cbeba503410c815356ab7358476dcaf38e
pauldin91 <pauldin91@gmail.com>
2026-06-29 11:20:04 +0300

    draft: board

--- lib/game_of_life_web/game_live/index.ex
+++ lib/game_of_life_web/game_live/index.ex
@@ -1,15 +1,16 @@
-defmodule GameOfLifeWeb.GameLive do
-   use GameOfLifeWeb, :live_view
+defmodule GameOfLifeWeb.GameLive.Index do
+  use GameOfLifeWeb, :live_view
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :board, GameOfLife.Engine.new_board(4))}
+    {:ok, assign(socket, :board, GameOfLife.Engine.new_board(8))}
   end
 
   @impl true
-  def handle_event() do
+  def handle_event("next", _map, socket) do
+    {:noreply, assign(socket, :board, GameOfLife.Engine.new_board(9))}
   end
 
-  @impl true
-  def handle_info() do
-  end
+  # @impl true
+  # def handle_info() do
+  # end
 end
--- lib/game_of_life_web/router.ex
+++ lib/game_of_life_web/router.ex
@@ -17,7 +17,7 @@ defmodule GameOfLifeWeb.Router do
   scope "/", GameOfLifeWeb do
     pipe_through :browser
 
-    live "/", GameOfLifeWeb.GameLive
+    live "/", GameLive.Index, :index
   end
 
   # Other scopes may use custom stacks.
